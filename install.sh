6185870365df80cc7cd7d26837dea84698b3e465
StafLoker <dev.stafloker@gmail.com>
2024-12-21 14:02:54 +0100

    fix: Extract the downloaded tar.gz file into the installation directory

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@
 RED='\033[31m'
 YELLOW='\033[33m'
 GREEN='\033[32m'
+PURPLE='\033[36m'
 RESET='\033[0m'
 
 # Function to print INFO messages
@@ -19,6 +20,11 @@ log_success() {
 # Function to print ERROR messages
 log_error() {
     echo -e "${RED}[ERROR] $1${RESET}"
+}
+
+# Function to print WARNING messages
+log_warning() {
+    echo -e "${PURPLE}[WARNING] $1${RESET}"
 }
 
 # Check if a version is provided, if not set a default version (latest)
@@ -43,14 +49,12 @@ install_dir="$HOME/ddns-porkbun-script"
 if [ ! -d "$install_dir" ]; then
     log_info "Creating directory: $install_dir"
     mkdir -p "$install_dir"
-else
-    log_success "Directory $install_dir already exists."
 fi
 
 # Download the specific version's tar.gz file
 url="https://github.com/StafLoker/ddns-porkbun-script/archive/refs/tags/${VERSION}.tar.gz"
 log_info "Downloading version ${VERSION} from $url"
-wget -N --no-check-certificate -O "${install_dir}/ddns-porkbun-script-${VERSION}.tar.gz" ${url}
+wget --no-check-certificate -P "${install_dir}" "${url}"
 
 # Check if the download was successful
 if [[ $? -ne 0 ]]; then
@@ -60,21 +64,27 @@ fi
 
 log_info "Extracting the downloaded tar.gz file..."
 # Extract the downloaded tar.gz file into the installation directory
-tar -xzvf "${install_dir}/ddns-porkbun-script-${VERSION}.tar.gz" -C "$install_dir"
-rm -f "${install_dir}/ddns-porkbun-script-${VERSION}.tar.gz"
+tar -xzvf "${install_dir}/${VERSION}.tar.gz" -C "$install_dir"
+rm -f "${install_dir}/${VERSION}.tar.gz"
 
 # Move the extracted files to the correct location
-mv "${install_dir}/ddns-porkbun-script-${VERSION}"/* "$install_dir/"
-rmdir "${install_dir}/ddns-porkbun-script-${VERSION}"
-
-log_info "Removing unwanted files (.git, .gitignore)..."
-# Remove .git and .gitignore if they exist in the install directory
-rm -rf "${install_dir}/.git" "${install_dir}/.gitignore"
+if [ -f "${install_dir}/ddns-porkbun-script-${VERSION#v}/ddns-porkbun-script.sh" ] && \
+   [ -f "${install_dir}/ddns-porkbun-script-${VERSION#v}/LICENSE" ] && \
+   [ -f "${install_dir}/ddns-porkbun-script-${VERSION#v}/README.md" ]; then
+    mv "${install_dir}/ddns-porkbun-script-${VERSION#v}/ddns-porkbun-script.sh" \
+       "${install_dir}/ddns-porkbun-script-${VERSION#v}/LICENSE" \
+       "${install_dir}/ddns-porkbun-script-${VERSION#v}/README.md" \
+       "$install_dir/"
+else
+    log_error "One or more files are missing in the source directory."
+    exit 1
+fi
+rm -rf "${install_dir}/ddns-porkbun-script-${VERSION#v}"
 
 log_info "Checking for keys.env file..."
 # Create the keys.env file if it doesn't exist in the install directory
 if [ ! -f "${install_dir}/keys.env" ]; then
-    log_info "Creating keys.env file..."
+    log_info "- Creating keys.env file..."
     echo 'PORKBUN_API_KEY="pk"' > "${install_dir}/keys.env"
     echo 'PORKBUN_SECRET_API_KEY="sk"' >> "${install_dir}/keys.env"
     chmod 600 "${install_dir}/keys.env"
@@ -85,7 +95,7 @@ fi
 log_info "Checking for data.json file..."
 # Create the data.json file if it doesn't exist in the install directory
 if [ ! -f "${install_dir}/data.json" ]; then
-    log_info "Creating data.json file..."
+    log_info "- Creating data.json file..."
     cat <<EOF > "${install_dir}/data.json"
 {
     "domain": "example.com",
@@ -116,3 +126,5 @@ log_info "Making the main script executable..."
 chmod +x "${install_dir}/ddns-porkbun-script.sh"
 
 log_success "Installation or update of ddns-porkbun-script version $VERSION completed successfully!"
+
+log_warning "Please configure the keys.env and data.json files before use."
